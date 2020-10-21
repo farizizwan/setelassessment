324f91f1b718bb5ed7b0e855552631cb059c20ad
Fariz <ifarizishak@gmail.com>
2020-10-21 14:02:48 +0800

    Update problem 1 for csv output

--- problem1/__main__.py
+++ problem1/__main__.py
@@ -14,12 +14,23 @@ def alternate(strInput):
             result += v.lower()
     return result
 
+def createcsv(strInput):
+    with open('problem1.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        temarr = []
+        for c in strInput:
+            temarr.append(c)
+        writer.writerow(temarr)
+    return "CSV Created!"
+
 def main():
     strInput = input("Sample input:")
     resutl = converttoupper(strInput)
     print(resutl)
     result2 = alternate(strInput)
     print(result2)
+    result3 = createcsv(strInput)
+    print(result3)
 
 if __name__ == '__main__':
     main()
